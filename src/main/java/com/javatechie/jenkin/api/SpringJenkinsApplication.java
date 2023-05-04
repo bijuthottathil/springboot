23d2b85f6af62812bdc693a5f1300d2e2f9f40c1
Biju Mathew Thottathil <63364506+bijuthottathil@users.noreply.github.com>
2023-05-04 14:26:56 -0500

    Update SpringJenkinsApplication.java
    modified

--- src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -12,7 +12,7 @@ public class SpringJenkinsApplication {
 
 	@GetMapping("/ping")
 	public String message() {
-		return "Wao!! Application Deployed successfully in SAP Cloud.biju.";
+		return "Wao!! Application Deployed successfully in SAP Cloud.Tendai.";
 	}
 
 	public static void main(String[] args) {
